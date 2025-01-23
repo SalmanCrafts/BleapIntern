91d6c57c2d4e7d1b19ebbcc1afc93a96778cc62e
Mohammed Salman <144619480+SalmanCrafts@users.noreply.github.com>
2025-01-23 12:08:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,27 +9,27 @@ function toggleAnswer(button) {
   }
 }
 
-// const Resultout = document.getElementById("time_show");
-// const SignupButton = document.getElementById("timeout_Register_btn");
-// const duration = 30;
-// let remainingTime = duration;
-// const messageElement = document.getElementById("message");
+const Resultout = document.getElementById("time_show");
+const SignupButton = document.getElementById("timeout_Register_btn");
+const duration = 30;
+let remainingTime = duration;
+const messageElement = document.getElementById("message");
 
-// function formatTime(seconds) {
-//   const minutes = Math.floor(seconds / 60);
-//   const secs = seconds % 60;
-//   return `${String(minutes).padStart(2, "0")}:${String(secs).padStart(2, "0")}`;
-// }
+function formatTime(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const secs = seconds % 60;
+  return `${String(minutes).padStart(2, "0")}:${String(secs).padStart(2, "0")}`;
+}
 
-// const countdown = setInterval(() => {
-//   Resultout.textContent = formatTime(remainingTime);
+const countdown = setInterval(() => {
+  Resultout.textContent = formatTime(remainingTime);
 
-//   if (remainingTime <= 0) {
-//     clearInterval(countdown);
-//     Resultout.textContent = "00:00";
-//     messageElement.textContent = "Time's up!";
-//     SignupButton.style.display = "none";
-//   } else {
-//     remainingTime--;
-//   }
-// }, 1000);
+  if (remainingTime <= 0) {
+    clearInterval(countdown);
+    Resultout.textContent = "00:00";
+    messageElement.textContent = "Time's up!";
+    SignupButton.style.display = "none";
+  } else {
+    remainingTime--;
+  }
+}, 1000);
